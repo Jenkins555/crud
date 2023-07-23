3113d04ea477361f9f9575c204404dcea3092235
Анисимов Александр <ujc90@mail.ru>
2023-07-23 07:26:49 +0300

    crud

---
+++ src/component/NotesList.js
@@ -0,0 +1,26 @@
+// NotesList.js
+
+import React from 'react';
+import './css/style.css'; // Подключаем общий файл стилей
+
+/**
+ * Компонент для отображения списка заметок.
+ *
+ * @param {Object} props - Свойства компонента.
+ * @param {Array} props.notes - Список заметок.
+ * @param {function} props.onDelete - Функция для удаления заметки.
+ */
+const NotesList = ({ notes, onDelete }) => {
+  return (
+    <div className="notes-list">
+      {notes.map((note) => (
+        <div key={note.id} className="note-card">
+          <p>{note.content}</p>
+          <button onClick={() => onDelete(note.id)}>Удалить</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NotesList;
